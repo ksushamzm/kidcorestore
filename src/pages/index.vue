e036361f65a8ceb84c998b8af8320fb8439916b7
ksushamzm <kalemudkina@edu.hse.ru>
2024-03-25 19:26:38 +0300

    главная

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,38 +3,34 @@
         <h2>Рекомендованные</h2>
         <v-row>
             <v-col 
-                v-for="product in smartphones"
+                v-for="product in forYou"
                 span="3"
                 md="6"
-                sm="12"
-            >
+                sm="12">
+
                 <v-catalog-card 
                     :id="product.id"
                     :image="product.image"
                     :title="product.title"
-                    :category="product.categoryId"
-                    :is-favorites="product.isFavorites"
-                    @addToFavorites="onToggleFavorites"
-                />
+                    :price="product.price"
+                    :category="product.categoryId"/>
             </v-col>
         </v-row>
     </v-container>
 </template>
 
 <script setup>
-    import { useCatalogCategory, useFavorites } from '@/composables';
+    import { useCatalogCategory} from '@/composables';
     import VContainer from '@/components/VContainer.vue';
     import VRow from '@/components/UI/VRow.vue';
     import VCol from '@/components/UI/VCol.vue';
     import VCatalogCard from '@/components/VCatalogCard.vue';
 
-    const { onToggleFavorites } = useFavorites();
-
     const { 
-        products: smartphones, 
-        getProductsCategory: getProductsCategorySmartphones
+        products: forYou, 
+        getProductsCategory: getProductsCategoryForYou
     } = useCatalogCategory();
 
-    getProductsCategorySmartphones(1);
+    getProductsCategoryForYou(2);
 
 </script>
